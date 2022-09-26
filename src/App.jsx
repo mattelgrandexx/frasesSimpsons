682692ca1ae2911f729d7bae77d0237988486c3f
matias cardoso <mattiascardoso10@gmail.com>
2022-09-26 11:38:21 -0300

    agregamos la promesa

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import './App.css';
 import LogoSimpsons from './LogoSimpsons';
 import CardHomero from "./CardHomero"
 import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+ const [personaje, setPersonaje]= useState({});
+
+  useEffect(()=>{
+    // la consulta a la API en este caso solo ocurre en el montaje por el corchete vacio
+    consultarApi();
+    
+},[])
+
+const consultarApi=()=>{
+try {
+  // el metodo fetch sirve para hacer peticiones a APIs
+  const respuesta = fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
+  console.log(respuesta);
+} catch (error) {
+  console.log(error)
+  // mostrar un mensaje de error al usuario
+}
+}
   return (
     <Container className='borde1 ancho fondo'>
 
